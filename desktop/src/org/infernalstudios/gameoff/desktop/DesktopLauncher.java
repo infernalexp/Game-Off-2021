7bc8e434805e5089f80dd33aaab099401e7d78bc
SwanX1 <karliscern@gmail.com>
2021-11-01 22:14:52 +0200

    Initial commit, set up LibGDX

---
+++ desktop/src/org/infernalstudios/gameoff/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.infernalstudios.gameoff.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.infernalstudios.gameoff.GameOff;
+
+public class DesktopLauncher {
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new GameOff(), config);
+    }
+}
